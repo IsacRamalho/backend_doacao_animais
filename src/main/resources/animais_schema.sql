fa70e2253cf6ff367569c484345c7d3891cdf855
IsacRamalho <isacramalho.ramalho@gmail.com>
2025-06-08 13:45:56 -0300

    Banco de Dados ATT

--- src/main/resources/animais_schema.sql
+++ src/main/resources/animais_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE animais(
+CREATE TABLE IF NOT EXISTS animais(
     id SERIAL PRIMARY KEY,
     nome TEXT NOT NULL,
     imagem TEXT,
@@ -8,4 +8,4 @@ CREATE TABLE animais(
     statusadocao TEXT,
     descr TEXT
 );
-INSERT INTO animais (nome, tipo, idade, raca, statusadocao, imagem, descr) VALUES ('Rex', 'Cachorro', 3, 'Labrador', 'Disponível', 'https://upload.wikimedia.org/wikipedia/commons/2/26/YellowLabradorLooking_new.jpg', 'Cachorro amigável e brincalhão.');
+INSERT INTO animais (nome, tipo, idade, raca, statusadocao, imagem, descr) SELECT 'Rex', 'Cachorro', 3, 'Labrador', 'Disponível', 'https://upload.wikimedia.org/wikipedia/commons/2/26/YellowLabradorLooking_new.jpg', 'Cachorro amigável e brincalhão.' WHERE NOT EXISTS (SELECT 1 FROM ANIMAIS);
